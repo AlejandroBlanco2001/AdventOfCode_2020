06ae1c595b44873ca8b036fec371a56c5c671a11
AlejandroBlanco2001 <nuclearhands2001@gmail.com>
2020-12-23 14:04:30 -0500

    Day 8 2/2

---
+++ Day 8/index.js
@@ -0,0 +1,55 @@
+function followInstructions(instructions){
+    var acc = 0
+    var i = 0
+    let steps = new Array(instructions.length)
+    while(!steps[i]){
+        if(instructions[i][0] == 'acc'){
+            acc += parseInt(instructions[i][1])
+            steps[i] = true
+            if(i+1 >= instructions.length)
+                return {acc: acc, mode: 'succesfull'}
+            else
+                i++
+        }else if(instructions[i][0] == 'jmp'){
+            steps[i] = true
+            i += parseInt(instructions[i][1])
+            if(i >= instructions.length){
+                return {acc: acc, mode: 'succesfull'}
+            }
+        }else{ 
+            i++
+        }
+    }
+    return {acc: acc, mode: 'loop'}
+}
+
+// Generate all the posibles changes, each per jmp or nop line
+function generateSimulations(lines){
+    var simulations = []
+    for(let i = 0; i < lines.length; i++){
+            if(lines[i][0] != 'acc'){
+                var simulation =  JSON.parse(JSON.stringify(lines))
+                simulation[i][0] = simulation[i][0] == 'nop' ? 'jmp' : 'nop' 
+                simulations.push(simulation)
+            }
+    }
+    return simulations 
+}
+
+async function fetchData(){
+    const req = await fetch('./input.txt')
+    const data = await req.text()
+    const lines = data.split('\n').map((item) => {return item.split(' ')}, [])
+    const part1 = followInstructions(lines)
+    const part2 = generateSimulations(lines).map((item) => {
+        let sim = followInstructions(item) 
+        if(sim.mode == "succesfull"){
+            console.log(sim.acc) 
+        }
+    })
+    return {part1: part1, part2: part2}
+}
+
+fetchData().then(data => {   
+    console.log(data)   
+})
