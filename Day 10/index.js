fc834b60d2f65af5ac16ee191b973eaf26ace61a
AlejandroBlanco2001 <nuclearhands2001@gmail.com>
2020-12-25 17:00:33 -0500

    Day 10 1/ refactor

--- Day 10/index.js
+++ Day 10/index.js
@@ -1,38 +1,22 @@
-function checkChain(batterys,max){
-    var oneJoin, twoJoin, threeJoin
-    var queue = []
-    var actualBattery = 0 
-    oneJoin = twoJoin = threeJoin = 0
-    while(actualBattery < max){ 
-        if(batterys[actualBattery + 1]){
-            if(!queue.includes(actualBattery + 1))
-                queue.push(actualBattery+1)
-            oneJoin++
-        }else if(batterys[actualBattery + 2]){
-            if(!queue.includes(actualBattery + 2))
-                queue.push(actualBattery+2)
-            twoJoin++
-        }else{
-            if(!queue.includes(actualBattery + 3))
-                queue.push(actualBattery+3)
-            threeJoin++
-        }
-        actualBattery = queue.shift()
+function diffCounter(array,diff){
+    var count = 0
+    for(let i = 0; i < array.length - 1; i++){
+        var curr = array[i]
+        var next = array[i+1]
+        if(next - curr == diff) count++
     }
-    return oneJoin * threeJoin
+    return count
 }
-
 
 async function fetchData(){
     const req = await fetch('./sample.txt')
     const data = await req.text()
     const lines = data.split('\n').map((item) => { return parseInt(item.replace(/\s/g, ''))})
-    const max = Math.max(...lines) + 3
-    const listBatterys = lines.reduce((acc,item) => {
-        acc[item] = item
-        return acc
-    },{})
-    return checkChain(listBatterys,max)
+    const sortedNumbers = lines.sort((a,b) => a-b)
+    const last = sortedNumbers[sortedNumbers.length-1]
+    const input = [0, ...sortedNumbers, last+3]
+    console.log(input)
+    return diffCounter(input,1) * diffCounter(input,3)
 }
 
 fetchData().then(data => {   
