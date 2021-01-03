86b0f68ad1b57293d4fe8c6303411ece7f2977bd
AlejandroBlanco2001 <nuclearhands2001@gmail.com>
2021-01-03 16:29:08 -0500

    Day 14 2/2 can be optimmized

--- Day 14/index.js
+++ Day 14/index.js
@@ -8,13 +8,19 @@ function getSumWithMask(value) {
         binarie = '0' + binarie
     }
     for(let i = 0; i < mask.length; i++){
-        if(mask[i] != 'X')
-            result = result + mask[i] 
-        else
-            result = result + binarie[i] 
-        
+        if(mask[i] != 'X'){
+            // Part 1 result = result + mask[i] 
+            if(mask[i] == 0)
+                result = result + binarie[i]
+            else
+                result = result + mask[i]
+        }else{
+            // Part 1 result = result + binarie[i] 
+            result = result + mask[i]
+        } 
     }
-    return parseInt(result,2)
+    // Part 1 return parseInt(result,2)
+    return result.replace(/^0+/,'')
 }
 
 function sumOfValues(memory){
@@ -27,14 +33,36 @@ function sumOfValues(memory){
     return cont
 }
 
+function generatePosibleCombinations(value){
+    if(value.length === 0)
+        return ['']
+
+    var firstChar = value[0]
+    var restValue = value.slice(1)
+    var partialValue = generatePosibleCombinations(restValue)
+    if(firstChar === 'X'){
+        return [
+        ...partialValue.map(item => '0' + item),
+        ...partialValue.map(item => '1' + item)
+        ]
+    }else{
+        return partialValue.map(item => firstChar + item)
+    }
+}
+
 function getMask(ins){
     var memory = {}
     var cont = 0
+    var masks = {}
     for(let i = 0; i < ins.length; i++){
         if(ins[i].length == 1){
             mask = ins[i][0]
         }else{
-            memory[ins[i][0]] = getSumWithMask(ins[i][1])
+            // Part 1 memory[ins[i][0]] = getSumWithMask(ins[i][1])
+            masks = generatePosibleCombinations(getSumWithMask(ins[i][0]))
+            for(let j = 0; j < masks.length; j++){
+                memory[parseInt(masks[j],2)] = ins[i][1]
+            }
         }
     }
     cont = sumOfValues(memory)
